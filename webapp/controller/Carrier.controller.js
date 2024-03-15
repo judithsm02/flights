29b5c31bc71b2bf5b7172ec3a62756aca30e6181
Judith Sanchez <judith.sanchez-marty.external@capgemini.com>
2024-03-15 13:03:04 +0000

    last commit

--- webapp/controller/Carrier.controller.js
+++ webapp/controller/Carrier.controller.js
@@ -8,8 +8,25 @@ sap.ui.define([
         "use strict";
 
         return Controller.extend("student00.com.sap.training.ux402.fullscreen.ux402fullscreen.controller.Carrier", {
-            onInit: function () {
+           
+                getRouter: function () {
+    
+                    return sap.ui.core.UIComponent.getRouterFor(this);
+                },
 
-            }
+           
+                onPress: function(oEvent) {
+
+                    var oItem = oEvent.getSource();
+                    var oCtx = oItem.getBindingContext();
+                    var sCarrid = oCtx.getProperty("Carrid");
+    
+                    this.getRouter().navTo("flights", {
+                        carrid:sCarrid //dins l'objecte flights se li passa el ID del carrier 
+                    },false); //controla si la nova vista ha de susbtituir la vista actual a l'historial del navegador, en aquest cas SI. 
+    
+                     //El false o true es el parametre bReplace que té laa funcion NavTo. 
+    
+                }
         });
     });
--- webapp/controller/NotFound.controller.js
+++ webapp/controller/NotFound.controller.js
@@ -1,14 +1,30 @@
-sap.ui.define(
-    [
-        "sap/ui/core/mvc/Controller"
-    ],
-    function(BaseController) {
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller) {
       "use strict";
-  
-      return BaseController.extend("student00.com.sap.training.ux402.fullscreen.ux402fullscreen.controller.NotFound", {
-        onInit: function() {
-        }
+
+      return Controller.extend("student00.com.sap.training.ux402.fullscreen.ux402fullscreen.controller.NotFound", {
+          getRouter: function() {
+              return sap.ui.core.UIComponent.getRouterFor(this);
+          },
+
+          onNavBack: function() {
+              var oHistory, sPreviousHash;
+              
+              oHistory = sap.ui.core.routing.History.getInstance();
+              sPreviousHash = oHistory.getPreviousHash();
+
+              if (sPreviousHash !== undefined) {
+                  window.history.go(-1);
+              } else {
+                  this.getRouter().navTo("overview", true /*no history*/);
+              }
+              
+          }
+
       });
-    }
-  );
-  
+  });
